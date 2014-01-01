a8431de964c027ab6848c2ba3776dc03c7b06181
Oleksandr Shtykhno <yukan@ukr.net>
2014-01-01 20:36:37 +0100

    Added URL for cloning and updating koans storage

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,11 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def koans = Action {
+    // clone or sync koans from git
+    // get list of the sources with koans
+    // parse koans and save to storage
+    Ok(views.html.koans())
+  }
+
 }
