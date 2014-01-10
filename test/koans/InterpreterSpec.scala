d7143fc7fe20202b2dec52c99cb92960837a46c6
oshtykhno <oshtykhno@asset-control.com>
2014-01-10 10:40:01 +0100

    - updated test

--- test/koans/InterpreterSpec.scala
+++ test/koans/InterpreterSpec.scala
@@ -11,34 +11,43 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class InterpreterSpec extends Specification {
 
-//  private val writer = new java.io.StringWriter()
-//  private val interpreter = new ScalaInterpreter(new Settings(),new PrintWriter(writer));
-//
-//  def exec(code : String) {
-//    // Clear the previous output buffer
-//    writer.getBuffer.setLength(0)
-//
-//    // Execute the code and catch the result
-//    val ir = interpreter.interpret(code);
-//
-//    // Return value or throw an exception based on result
-//    ir match {
-//      case Success => ()
-//      case Error => throw new ScriptException("error in: '" +
-//        code + "'\n" + writer toString)
-//      case Incomplete => throw new ScriptException("incomplete in :'" +
-//        code + "'\n" + writer toString)
-//    }
-//  }
+  val source = """import org.scalatest.matchers.ShouldMatchers
+import support.KoanSuite
 
+// meditate on AboutAsserts to see how the Scala Koans work
+class AboutAsserts extends KoanSuite with ShouldMatchers {
+
+  koan("asserts can take a boolean argument") {
+    assert(false) // should be true
+  }
+
+  koan("asserts can include a message") {
+    assert(false, "This should be true")
+  }
+
+  koan("true and false values can be compared with should matchers") {
+    true should be(__) // should be true
+  }
+
+  koan("booleans in asserts can test equality") {
+    val v1 = 4
+    val v2 = 4
+    v1 === __ // === is an assert. It is from ScalaTest, not from the Scala language
+  }
+
+  koan("sometimes we expect you to fill in the values") {
+    assert(__ == 1 + 1)
+  }
+}"""
+  
+  
 
   "Koan Interpreter" should {
 
     "test koan interpret" in {
-       val source = """ """
 
 
-       source.length === 1
+       source.length === 742
     }
 
   }
