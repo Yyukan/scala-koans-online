0c923fa8e9ce601daba05e612fabcb1abe3b4c64
Oleksandr Shtykhno <yukan@ukr.net>
2014-02-26 19:19:39 +0100

    - added test which interpret and execute scalatest code dynamically

--- test/koans/InterpreterSpec.scala
+++ test/koans/InterpreterSpec.scala
@@ -3,6 +3,8 @@ package koans
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
+import scala.tools.nsc.interpreter.IMain
+import java.io.File
 
 
 /**
@@ -39,15 +41,55 @@ class AboutAsserts extends KoanSuite with ShouldMatchers {
     assert(__ == 1 + 1)
   }
 }"""
-  
+
+  val source1 =
+    """
+      import org.scalatest.FlatSpec
+
+      class SetSpec extends FlatSpec {
+
+        "An empty Set" should "have size 0" in {
+          assert(Set.empty.size == 0)
+        }
+
+        it should "produce NoSuchElementException when head is invoked" in {
+          intercept[NoSuchElementException] {
+            Set.empty.head
+          }
+        }
+      }
+
+
+    """
   
 
   "Koan Interpreter" should {
 
-    "test koan interpret" in {
+    "test interpret simple expression and scalatest" in {
+      import scala.tools.nsc._
+      import scala.tools.nsc.util.ClassPath
 
 
-       source.length === 742
+      val settings = new Settings
+      settings.bootclasspath.value +=
+      ClassPath.join(
+         scala.tools.util.PathResolver.Environment.javaBootClassPath + File.pathSeparator + "lib/scala-library.jar",
+         scala.tools.util.PathResolver.Environment.javaBootClassPath + File.pathSeparator + "lib/scalatest_2.10-2.0.jar" )
+
+
+      val in = new IMain(settings){
+        override protected def parentClassLoader = settings.getClass.getClassLoader()
+      }
+      val res = in.interpret("val x = 1; case class A {}")
+      println("Result " + res)
+
+      in.interpret(source1) match {
+        case x  => println(x)
+      }
+
+      val output = in.interpret("(new SetSpec).execute()")
+      println("Output" + output)
+      source.length === 742
     }
 
   }
